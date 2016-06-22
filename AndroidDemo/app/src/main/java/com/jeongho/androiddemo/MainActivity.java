2caa98ecdc2b2960223ad7357dde05bb3339e52c
JeonghoWin <zhfighting2014@163.com>
2016-06-22 16:47:46 +0800

    init

---
+++ AndroidDemo/app/src/main/java/com/jeongho/androiddemo/MainActivity.java
@@ -0,0 +1,53 @@
+package com.jeongho.androiddemo;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.jeongho.androiddemo.fragment.FirstFragment;
+import com.jeongho.androiddemo.fragment.SecondFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fm;
+    private FirstFragment ff;
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+        setContentView(R.layout.activity_main);
+
+        ff = new FirstFragment();
+        fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.add(R.id.fl_content, ff);
+        transaction.commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    public void changeFragment(View v){
+        FragmentTransaction transaction = fm.beginTransaction();
+        SecondFragment sf = new SecondFragment();
+//        transaction.replace(R.id.fl_content, sf);
+//        transaction.commit();
+        if (ff.isAdded()){
+            transaction.hide(ff).add(R.id.fl_content, sf).commit();
+        }
+
+    }
+}
